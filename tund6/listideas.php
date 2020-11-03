84d62a35174058d4e1c0233a208a429cdcaaed7c
Margo Naroskin <margonaroskin@users.noreply.github.com>
2020-11-03 22:10:04 +0200

    kuuenda tunni töö

---
+++ tund6/listideas.php
@@ -0,0 +1,44 @@
+<?php
+  require("usesession.php");
+  //loeme andmebaasi login ifo muutujad
+  require("../../../config.php");
+  //kui kasutaja on vormis andmeid saatnud, siis salvestame andmebaasi
+  $database = "if20_margo_nar_2";
+    
+  //loeme andmebaasist
+  $ideahtml = "";
+  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+  //valmistame ette SQL käsu
+  $stmt = $conn->prepare("SELECT idea FROM myideas");
+  echo $conn->error;
+  //seome tulemuse mingi muutujaga
+  $stmt->bind_result($ideafromdb);
+  $stmt->execute();
+  //võtan, kuni on
+  while($stmt->fetch()){
+	  //<p>suvaline mõte </p>
+	  $ideahtml .= "<p>" .$ideafromdb ."</p>";
+  }
+  $stmt->close();
+  $conn->close();
+  //ongi andmebaasisit loetud
+
+  //$username = "Margo Narõškin";
+
+  require("header.php");
+?>
+
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+    
+  <ul>
+    <li><a href="home.php">Avalehele</a></li>
+	<li><a href="?logout=1">Logi välja</a>!</li>
+  </ul>
+  
+  <hr>
+  <?php echo $ideahtml; ?>
+</body>
+</html>
