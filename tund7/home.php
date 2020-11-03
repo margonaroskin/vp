93238f3d398fa00976cca5e50df7991a1e799c35
Margo Naroskin <margonaroskin@users.noreply.github.com>
2020-11-03 22:10:45 +0200

    seitsmenda tunni töö

---
+++ tund7/home.php
@@ -0,0 +1,28 @@
+<?php
+	//var_dump($_POST);
+	require("usesession.php");
+  
+	//$username = "Margo Narõškin";
+  
+  require("header.php");
+?>
+
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursuse raames <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <p><a href="?logout=1">Logi välja</a>!</p>
+  <ul>
+    <li><a href="addideas.php">Oma mõtete salvestamine</a></li>
+	<li><a href="listideas.php">Mõtete vaatamine</a></li>
+	<li><a href="listfilms.php">Filmide nimekirja vaatamine</a></li>
+	<li><a href="addfilms.php">Filmiinfo lisamine</a></li>
+	<li><a href="addfilmrelations.php">Filmiinfo seoste lisamine</a></li>
+	<li><a href="listfilmpersons.php">Filmitegelaste loend</a></li>
+	<li><a href="userprofile.php">Minu kasutajaprofiil</a></li>
+  </ul>
+  
+  <hr>
+
+</body>
+</html>
---
+++ tund7/listfilms.php
@@ -0,0 +1,28 @@
+<?php
+require("usesession.php");
+//loeme andmebaasi login ifo muutujad
+require("../../../config.php");
+//kui kasutaja on vormis andmeid saatnud, siis salvestame andmebaasi
+//$database = "if20_margo_nar_2";
+require("fnc_film.php");
+
+//$username = "Margo Narõškin";
+
+require("header.php");
+?>
+
+<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
+<h1><?php echo $_SESSION["userfirstname"] . " " . $_SESSION["userlastname"]; ?></h1>
+<p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+<p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate
+    instituudis.</p>
+
+<ul>
+    <li><a href="home.php">Avalehele</a></li>
+    <li><a href="?logout=1">Logi välja</a>!</li>
+</ul>
+
+<hr>
+<?php echo readfilms(); ?>
+</body>
+</html>
